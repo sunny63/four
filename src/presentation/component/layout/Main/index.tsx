7651b246429d4d4d17111c8bc5b8c59952261de8
Dmitry Fedorenko <fedorenko@taptima.ru>
2021-10-01 18:12:20 +1000

    Make form

--- src/presentation/component/layout/Main/index.tsx
+++ src/presentation/component/layout/Main/index.tsx
@@ -1,15 +1,16 @@
 import React, { FC } from 'react';
 import { Global } from '@emotion/react';
 import globalCss from 'presentation/component/layout/common/globalCss';
+import { Wrapper } from './styles'
 
 const Layout: FC = (props) => {
     const { children } = props;
 
     return (
-        <>
+        <Wrapper>
             {children}
             <Global styles={globalCss} />
-        </>
+        </Wrapper>
     );
 };
 
---
+++ src/presentation/component/layout/Main/styles.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Wrapper = styled.div`
+    background-color: lightgray;
+    min-height: 100vh;
+  padding: 30px;
+`
