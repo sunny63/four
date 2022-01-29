60f1a6524724a32ec9cb9dd9f486a0e90adb56f2
Dmitry Fedorenko <fedorenko@taptima.ru>
2022-01-29 20:28:27 +1000

    Remove unnecessary code

--- src/presentation/component/page/home/Tables/index.tsx
+++ src/presentation/component/page/home/Tables/index.tsx
@@ -1,14 +1,10 @@
 import { FC, useState } from 'react';
 import { observer } from 'mobx-react';
-// import exportFromJSON, { ExportType } from 'export-from-json';
 import Box from '@material-ui/core/Box';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Button from '@material-ui/core/Button';
-// import PeriodForDownloading from 'domain/entity/period/PeriodForDownloading';
-// import AttributeForDownloading from 'domain/entity/attribute/AttributeForDownloading';
 import Step from 'domain/entity/app/Step';
-// import AmountOfPeriods from 'domain/entity/AmountOfPeriods';
 import { useService } from 'presentation/context/Container';
 import AppController from 'presentation/controller/app/AppController';
 import PeriodTable from './PeriodTable';
@@ -19,47 +15,15 @@ import CHPDTable from './CHPDTable';
 import { NavBox, ButtonsContainer } from './styles';
 
 const Tables: FC = observer(() => {
-    const {
-        // getAttributesForDownloading,
-        // getAmountOfPeriodsForDownloading,
-        // getPeriodsForDownloading,
-        setInputDataStep,
-        // periods,
-        // attributes,
-        step,
-        setIndKnowledgeBaseGenerationStep,
-    } = useService(AppController);
+    const { setInputDataStep, step, setIndKnowledgeBaseGenerationStep } = useService(AppController);
     const [value, setValue] = useState<number>(4);
     const buttonText = step === Step.IndKnowledgeBase ? 'Сформировать' : 'Генерация';
-    // const isDisabled = value <= 1;
     const generationButtonOnClick =
         step === Step.IndKnowledgeBase ? setIndKnowledgeBaseGenerationStep : setInputDataStep;
 
     const handleChange = (newValue: number) => {
         setValue(newValue);
     };
-
-    // const handleDownloadButtonClick = async (type: ExportType) => {
-    //     let data: AmountOfPeriods[] | PeriodForDownloading[] | AttributeForDownloading[] = [];
-    //
-    //     if (value === 2) {
-    //         data = await getAttributesForDownloading(attributes);
-    //     }
-    //
-    //     if (value === 3) {
-    //         data = await getAmountOfPeriodsForDownloading(periods);
-    //     }
-    //
-    //     if (value === 4) {
-    //         data = await getPeriodsForDownloading(periods);
-    //     }
-    //
-    //     exportFromJSON({
-    //         data,
-    //         fileName: 'data',
-    //         exportType: type,
-    //     });
-    // };
 
     return (
         <Box>
@@ -80,22 +44,6 @@ const Tables: FC = observer(() => {
                     <Button type="button" color="primary" onClick={generationButtonOnClick}>
                         {buttonText}
                     </Button>
-                    {/* <Button */}
-                    {/*    type="button" */}
-                    {/*    color="primary" */}
-                    {/*    disabled={isDisabled} */}
-                    {/*    onClick={() => handleDownloadButtonClick('csv')} */}
-                    {/* > */}
-                    {/*    Скачать CSV */}
-                    {/* </Button> */}
-                    {/* <Button */}
-                    {/*    type="button" */}
-                    {/*    color="primary" */}
-                    {/*    disabled={isDisabled} */}
-                    {/*    onClick={() => handleDownloadButtonClick('xls')} */}
-                    {/* > */}
-                    {/*    Скачать XLS */}
-                    {/* </Button> */}
                 </ButtonsContainer>
             </NavBox>
             {value === 0 && <DiseasesTable />}
